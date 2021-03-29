2c3b64dd8e48bbac6513c98d7b20c421be1065b3
Maggie WU <maggie@Maggies-MacBook-Pro.local>
2021-03-30 00:45:49 +0800

    add more test

--- TestSDF.cpp
+++ TestSDF.cpp
@@ -20,18 +20,57 @@ int main(int, const char* argv[]){
     code = provider.KeyGen(SM2,&key);
     cout << "****KeyGen****" << endl;
     cout << provider.GetErrorMessage(code) << endl;
-    cout <<key.PublicKey()<<endl;
+    PrintData("public key",key.PublicKey(),64,16);
 
     cout << "****Sign****" << endl;
     char * signature = (char *)malloc(64*sizeof(char));
     unsigned int len;
     provider.Sign(key,SM2,out,32,signature,&len);
-    cout <<signature<<endl;
+    cout << provider.GetErrorMessage(code) << endl;
+    PrintData("signature",signature,64,16);
 
     cout << "****Verify****" << endl;
     bool result;
-    provider.Verify(key,SM2,out,32,signature,64,&result);
-    cout <<result<<endl;
+    code = provider.Verify(key,SM2,out,32,signature,64,&result);
+    cout << provider.GetErrorMessage(code) << endl;
+    cout <<"verify result"<< result<<endl;
 
     return 0;
+}
+
+int PrintData(char *itemName, char *sourceData, unsigned int dataLength, unsigned int rowCount)
+{
+	int i, j;
+	
+	if((sourceData == NULL) || (rowCount == 0) || (dataLength == 0))
+		return -1;
+	
+	if(itemName != NULL)
+		printf("%s[%d]:\n", itemName, dataLength);
+	
+	for(i=0; i<(int)(dataLength/rowCount); i++)
+	{
+		printf("%08x  ",i * rowCount);
+
+		for(j=0; j<(int)rowCount; j++)
+		{
+			printf("%02x ", *(sourceData + i*rowCount + j));
+		}
+
+		printf("\n");
+	}
+
+	if (!(dataLength % rowCount))
+		return 0;
+	
+	printf("%08x  ", (dataLength/rowCount) * rowCount);
+
+	for(j=0; j<(int)(dataLength%rowCount); j++)
+	{
+		printf("%02x ",*(sourceData + (dataLength/rowCount)*rowCount + j));
+	}
+
+	printf("\n");
+
+	return 0;
 }
